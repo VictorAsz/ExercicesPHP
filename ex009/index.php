a28529c48aaa68e1e0d10ce8dc91050feb99ffaa
Victor Assunção <victor.a.schumann@gmail.com>
2023-11-28 17:13:16 -0300

    ex009 raizes

---
+++ ex009/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex009 Raizes</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+<header><h1>Informe um número </h1></header>
+<div class="container">
+    <?php 
+        $numero = $_GET['num'] ?? 0git;
+    ?>
+
+<main>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="num"> Número:</label>
+        <input type="number" name="num" id="num" value="<?=$numero?>" placeholder="Insira aqui seu número">
+        <button type="submit">Calcular</button>
+    </form>
+</main>
+</div>
+
+<div class="container">
+    <p>Resultado final</p>
+    <?php 
+        $raiz_quadrada = $numero ** (1/2);
+        $raiz_cubica = $numero ** (1/3);
+
+        echo "<p> Analisando o númmero <strong>$numero</strong>, temos:";
+        echo "<ul> <li>A sua raiz quadrada é " . number_format($raiz_quadrada, 2, ',', '.') . "</li>";
+        echo "<li>A sua raiz cúbica é " . number_format($raiz_cubica, 2,',','.') . "</li> </ul>";
+
+    ?>
+</div>
+
+
+
+</body>
+</html>
